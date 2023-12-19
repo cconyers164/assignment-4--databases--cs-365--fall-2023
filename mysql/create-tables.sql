e337f08768e2806fab5be76269ec8945b7c66d96
cconyers164 <chrconyers@gmail.com>
2023-12-19 18:48:16 -0500

    Fixed syntax and added more tables in create-tables.sql

--- mysql/create-tables.sql
+++ mysql/create-tables.sql
@@ -1,21 +1,32 @@
 CREATE TABLE IF NOT EXISTS Artist (
-  artist_id   INT          NOT NULL,
+  artist_id INT NOT NULL,
   artist_name VARCHAR(128) NOT NULL,
   PRIMARY KEY (artist_id)
 );
 
+CREATE TABLE IF NOT EXISTS compiles (
+  artist_name VARCHAR(128) REFERENCES Artist(artist_name),
+  album_name  VARCHAR(128) REFERENCES Album(album_name),
+  artist_id INT REFERENCES Artist(artist_id)
+);
+
 CREATE TABLE IF NOT EXISTS Album (
-  artist_id  INT          NOT NULL,
-  album_id   INT          NOT NULL,
+  artist_id  INT NOT NULL,
+  album_id   INT NOT NULL,
   album_name VARCHAR(128) NOT NULL,
-  PRIMARY KEY (album_id),
-  FOREIGN KEY (artist_id, album_id) REFERENCES Artist(artist_id, album_id)
+  PRIMARY KEY (artist_id, album_id),
+  FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+);
+
+CREATE TABLE IF NOT EXISTS contains (
+  album_name VARCHAR(128) REFERENCES Album(album_name),
+  track_name VARCHAR(128) REFERENCES Track(track_name)
 );
 
 CREATE TABLE IF NOT EXISTS Track (
-  artist_id  INT          NOT NULL,
-  album_id   INT          NOT NULL,
-  track_id   INT          NOT NULL,
+  artist_id  INT NOT NULL,
+  album_id   INT NOT NULL,
+  track_id   INT NOT NULL,
   track_name VARCHAR(128) NOT NULL,
   time       DECIMAL(5,2) NOT NULL,
   PRIMARY KEY (artist_id, album_id, track_id),
@@ -23,15 +34,14 @@ CREATE TABLE IF NOT EXISTS Track (
 );
 
 CREATE TABLE IF NOT EXISTS Played (
-  artist_id  INT          NOT NULL,
-  album_id   INT          NOT NULL,
-  track_id   INT          NOT NULL,
-  played     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (artist_id, album_id, track_id, played),
-  FOREIGN KEY (artist_id, album_id, track_id) REFERENCES Track(artist_id, album_id, track_id)
+  track_name VARCHAR(128)  REFERENCES contains(track_name),
+  played TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (played)
 );
 
-
+CREATE UNIQUE INDEX idx_Artist ON Artist(artist_id);
+CREATE UNIQUE INDEX idx_Album ON Album(artist_id, album_id);
+CREATE UNIQUE INDEX idx_Track ON Track(artist_id, album_id, track_id);
 
 
 
